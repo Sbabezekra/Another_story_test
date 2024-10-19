d6dbb718e285fac9e7587bc7b7d78fc1a70798d5
Sbabezekra <sbabezekra@gmail.com>
2024-10-19 14:41:21 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+print("Hello")
+input("> ")
+
+students = ["Yasha","Sasha"]
+students.append("Dima")
+students.remove("Dima")
+
+students = {"Yasha":
+                    ["72387328","72347239842"],
+            "Sasha": ["72387328","72347239842"]
+            }
+
+if name == Yasha:
+    print("ok")
+elif name == Sasha:
+    print("No")
+else:
+    print("NO")
+
+students = ["Yasha","Sasha"]
+
+if "Dima" not in students or "Grisha" in students:
+    print("True")
+else:
+    print("Error")
+
+
+while name == "Yasha":
+    print("Yasha is on group")
+
+marshutka = 0
+while marshutka<=5:
+    marshutka = marshutka + 1
+    print(marshutka)
+
+
+
+
+
